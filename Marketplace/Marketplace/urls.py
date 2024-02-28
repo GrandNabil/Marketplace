31838c86ca4dd324804b2a12bf9797d036a759b3
Grand Nabil <akambilawani@gmail.com>
2024-02-28 17:24:50 +0100

    settings up contact page

--- Marketplace/Marketplace/urls.py
+++ Marketplace/Marketplace/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path
-from market.views import index
+from market.views import index, contact
 urlpatterns = [
     path("", index, name="index"),
+    path("", contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
--- Marketplace/market/views.py
+++ Marketplace/market/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'market/index.html')
+
+def contact(request):
+    return render(request, 'market/contact.html')
