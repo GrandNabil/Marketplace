67610cc7a48d8682a52359e49913e0eee5f5b5b9
Grand Nabil <akambilawani@gmail.com>
2024-02-28 22:51:51 +0100

    items databse configuration

--- Marketplace/item/admin.py
+++ Marketplace/item/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Categorie, Item
 
-# Register your models here.
+admin.site.register(Categorie)
+admin.site.register(Item)
--- Marketplace/item/models.py
+++ Marketplace/item/models.py
@@ -1,5 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Categorie(models.Model):
     name =  models.CharField(max_length=255)
+
+    class Meta:
+        #ordering =  ('name',)
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    categori= models.ForeignKey(Categorie, related_name='items', on_delete=models.CASCADE)
+    name =  models.CharField(max_length=255)
+    description = models.TextField(blank = True, null=True)
+    prix = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    vendu = models.BooleanField(default=False)
+    ajoute_par = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    ajoute_le = models.DateTimeField(auto_now_add=True)
