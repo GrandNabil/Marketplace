8ed9ca84caad4bebe57e4ec105cd4527c5762131
Grand Nabil <akambilawani@gmail.com>
2024-02-29 15:05:45 +0100

    categorie modified by categori

--- Marketplace/item/views.py
+++ Marketplace/item/views.py
@@ -3,7 +3,9 @@ from .models import Item
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(categori=item.categori, vendu=False).exclude(pk=pk)[0:3]
 
     return render(request, 'item/detail.html', {
-        'item':item
+        'item':item,
+        'related_items': related_items
     })
